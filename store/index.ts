04f8c6d7c01603af9c16100972cf24fed64bd1d0
gmldnjs26 <imleesky265@gmail.com>
2021-05-22 18:05:10 +0900

    Maintain user information on refresh

--- store/index.ts
+++ store/index.ts
@@ -1,19 +1,19 @@
-
 import { ActionTree, Store } from 'vuex';
-import { ActionContext } from "vuex/types";
-import { Context } from "@nuxt/types";
+import { ActionContext } from 'vuex/types';
+// import { Context } from '@nuxt/types';
 import { initialiseStores } from '~/utils/store-accessor';
 
 export const state = () => ({});
-export type RootState = ReturnType<typeof state>
-
-// Rootのactionsを追加
-export const actions: ActionTree<any, any> = {
-  async nuxtServerInit({ dispatch }) {
-    return await dispatch('users/loadUser');
-  }
-}
+export type RootState = ReturnType<typeof state>;
 
 const initializer = (store: Store<any>) => initialiseStores(store);
 export const plugins = [initializer];
+
+// Rootのactionsを追加
+export const actions: ActionTree<any, any> = {
+	nuxtServerInit: async (context: ActionContext<RootState, RootState>) => {
+		await context.dispatch('users/loadUser');
+	},
+};
+
 export * from '~/utils/store-accessor';
